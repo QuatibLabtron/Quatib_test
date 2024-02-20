a51d8cc3996eb00a98cce5887d0db783858980c7
QuatibLabtron <quatib@labo.best>
2024-02-20 14:22:52 +0530

    Update pull.php

--- mednics.com/pull.php
+++ mednics.com/pull.php
@@ -1,12 +1,23 @@
-<?php
-function execPrint($command)
-{
-    $result = array();
-    exec($command, $result);
-    foreach ($result as $line) {
-        print($line . "<br>");
-    }
-}
-// Print the exec output inside of a pre element
-print("<pre>" . execPrint("git pull https://Admin063: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@github.com/Admin063/labotronics.com.git main") . "</pre>");
+<?php
+
+function execPrint($command)
+
+{
+
+    $result = array();
+
+    exec($command, $result);
+
+    foreach ($result as $line) {
+
+        print($line . "<br>");
+
+    }
+
+}
+
+// Print the exec output inside of a pre element
+
+print("<pre>" . execPrint("git pull https://Admin063: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@github.com/Admin063/mednics.com.git main") . "</pre>");
+
 ?>
